752a6a5d596355300bebce252914c6196d139684
Dipti-Coder202 <dipti.sahu.official@gmail.com>
2026-08-06 12:39:39 +0530

    postgressSql connection

---
+++ Project2_RetailSales_Postgres/README.md
@@ -0,0 +1,36 @@
+# Retail Sales ETL - PostgreSQL
+
+## Project Overview
+
+This project demonstrates an ETL pipeline using PySpark.
+
+### Source
+CSV File
+
+### Processing
+PySpark
+
+### Target
+PostgreSQL
+
+## Technologies
+
+- Python
+- PySpark
+- PostgreSQL
+- JDBC
+- VS Code
+
+## Flow
+
+CSV
+   ↓
+PySpark
+   ↓
+Transform
+   ↓
+PostgreSQL
+
+## Run
+
+python scripts/retail_to_postgres.py
---
+++ Project2_RetailSales_Postgres/scripts/retail_to_postgres.py
@@ -0,0 +1,49 @@
+from pyspark.sql import SparkSession
+
+# spark = (
+#     SparkSession.builder
+#     .appName("Retail To PostgreSQL")
+#     .getOrCreate()
+# )
+
+spark = (
+    SparkSession.builder
+    .appName("Retail To PostgreSQL")
+    .config(
+    "spark.jars",
+    "/Users/apple/Desktop/DataEngineering/Project2_RetailSales_Postgres/jars/postgresql-42.7.7.jar"
+)
+    .getOrCreate()
+)
+
+url = "jdbc:postgresql://localhost:5432/retail_db"
+
+properties = {
+    "user": "apple",
+    "password": "",
+    "driver": "org.postgresql.Driver"
+}
+import os
+
+base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+csv_path = os.path.join(base_path, "data", "sales.csv")
+
+print(csv_path)
+
+df = spark.read.csv(
+    csv_path,
+    header=True,
+    inferSchema=True
+)
+
+df.show()
+
+df.write.mode("overwrite").jdbc(
+        url=url,
+        table="retail_sales",
+        properties=properties
+    )
+
+print("Data Loaded Successfully!")
+
+spark.stop()
